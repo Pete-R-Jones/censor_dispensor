a8dc5439a87fa625b069bc22ef7d75021fbff1e1
Pete Jones <peterjones252@gmail.com>
2019-12-28 16:58:02 +0000

    Added function which substitutes out negative phrases once two have already appeared alongside other unwanted phrases. In its current form it only accepts 1, 2 or 3 word phrases

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -20,7 +20,7 @@ def censor(phrase, document):
         censored_doc = insensitive_phrase.sub(new_word, document)
     return censored_doc
 
-print(censor("learning algorithms", email_one))
+# print(censor("learning algorithms", email_one))
 
 def censor_many(phrases, document):
     censored_doc = document
@@ -38,4 +38,91 @@ def censor_many(phrases, document):
 
 proprietary_terms = ["she", "personality matrix", "sense of self", "self-preservation", "learning algorithm", "her", "herself"]
 
-print(censor_many(proprietary_terms, email_two))
+# print(censor_many(proprietary_terms, email_two))
+
+negative_words = ["concerned", "behind", "danger", "dangerous", "alarming", "alarmed", "out of control", "help", "unhappy", "bad", "upset", "awful", "broken", "damage", "damaging", "dismal", "distressed", "distressed", "concerning", "horrible", "horribly", "questionable"]
+
+def censor_3(censor_phrases, negative_words, document):
+    censored_doc = document
+    document_words = []
+    document_phrases_2 = []
+    document_phrases_3 = []
+    j = 0
+    for i in document.split():
+        k = i.split("\n")
+        for word in k:
+            document_words.append(word)
+    for i in range(len(document_words)-1):
+        document_phrases_2.append(document_words[i] + " " + document_words[i+1])
+    document_phrases_2.append("")
+    for i in range(len(document_words)-2):
+        document_phrases_3.append(document_words[i] + " " + document_words[i+1] + " " + document_words[i+2])
+    document_phrases_3.append("")
+    document_phrases_3.append("")
+    count = 0
+    while j < len(document_words):
+        new_word = ""
+        if document_words[j].lower().translate(str.maketrans('', '', string.punctuation)) in censor_phrases:
+            for letter in document_words[j]:
+                if (letter not in string.punctuation):
+                    new_word += "X"
+                else:
+                    new_word += letter
+            document_words[j] = new_word
+            j += 1
+        elif document_phrases_2[j].lower().translate(str.maketrans('', '', string.punctuation)) in censor_phrases:
+            temp = []
+            for letter in document_phrases_2[j]:
+                if (letter not in string.punctuation) and (letter not in " "):
+                    new_word += "X"
+                else:
+                    new_word += letter
+            temp = new_word.split()
+            document_words[j] = temp[0]
+            document_words[j+1] = temp[1]
+            j += 2
+        elif document_phrases_3[j].lower().translate(str.maketrans('', '', string.punctuation)) in censor_phrases:
+            temp = []
+            for letter in document_phrases_3[j]:
+                if (letter not in string.punctuation) and (letter not in " "):
+                    new_word += "X"
+                else:
+                    new_word += letter
+            temp = new_word.split()
+            document_words[j] = temp[0]
+            document_words[j+1] = temp[1]
+            document_words[j+2] = temp[2]
+            j += 3
+        elif (document_words[j].lower().translate(str.maketrans('', '', string.punctuation)) in negative_words) or (document_phrases_3[j].lower().translate(str.maketrans('', '', string.punctuation)) in negative_words):
+            count += 1
+            if count > 2:
+                if document_words[j].lower().translate(str.maketrans('', '', string.punctuation)) in negative_words:
+                    for letter in document_words[j]:
+                        if (letter not in string.punctuation):
+                            new_word += "X"
+                        else:
+                            new_word += letter
+                    document_words[j] = new_word
+                    j += 1
+                elif document_phrases_3[j].lower().translate(str.maketrans('', '', string.punctuation)) in negative_words:
+                    temp = []
+                    for letter in document_phrases_3[j]:
+                        if (letter not in string.punctuation) and (letter not in " "):
+                            new_word += "X"
+                        else:
+                            new_word += letter
+                    temp = new_word.split()
+                    document_words[j] = temp[0]
+                    document_words[j+1] = temp[1]
+                    document_words[j+2] = temp[2]
+                    j += 3
+            elif document_words[j].lower().translate(str.maketrans('', '', string.punctuation)) in negative_words:
+                j += 1
+            else:
+                j+= 3
+        else:
+            j += 1
+    return " ".join(document_words)
+
+print(censor_3(proprietary_terms, negative_words, email_three))
+
