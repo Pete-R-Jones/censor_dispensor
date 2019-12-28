e33dd838459b754459c2b001dc05917e7fc8023a
Pete Jones <peterjones252@gmail.com>
2019-12-28 13:17:01 +0000

    Added function that censors a phrase from a document

--- censor_dispenser.py
+++ censor_dispenser.py
@@ -3,3 +3,19 @@ email_one = open("email_one.txt", "r").read()
 email_two = open("email_two.txt", "r").read()
 email_three = open("email_three.txt", "r").read()
 email_four = open("email_four.txt", "r").read()
+
+import string
+
+def censor(phrase, document):
+    new_word = ""
+    censored_doc = document
+    if phrase.lower() in document.lower():
+        for letter in phrase:
+            if (letter not in string.punctuation) and (letter not in " "):
+                new_word += "X"
+            else:
+                new_word += letter
+        censored_doc = document.lower().replace(phrase.lower(), new_word)
+    return censored_doc
+
+print(censor("learning algorithms", email_one))
